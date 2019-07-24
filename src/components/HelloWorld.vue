7a9e06787d07a3b62e3812749712f27f51bb2926
Eddie Ferrer <eddie.ferrer@daimler.com>
2019-07-24 10:20:53 -0700

    Added initial files

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+    <ag-grid-vue
+      class="ag-theme-balham"
+      :columnDefs="columnDefs"
+      :rowData="rowData"
+    >
+      >
+    </ag-grid-vue>
+</template>
+
+<script>
+import { AgGridVue } from "ag-grid-vue";
+
+export default {
+  name: "HelloWorld",
+  components: { AgGridVue },
+  data() {
+    return {
+      columnDefs: null,
+      rowData: null
+    };
+  },
+  beforeMount() {
+    this.columnDefs = [
+      { headerName: "Make", field: "make" },
+      { headerName: "Model", field: "model" },
+      { headerName: "Price", field: "price" }
+    ];
+
+    this.rowData = [
+      { make: "Toyota", model: "Celica", price: 35000 },
+      { make: "Ford", model: "Mondeo", price: 32000 },
+      { make: "Porsche", model: "Boxter", price: 72000 }
+    ];
+  }
+};
+</script>
+
+<style scoped>
+</style>
